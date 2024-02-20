Adding Cardinal info, fixing e calculation bug in coop_setup_funcs

general_code/cat_to_numden_maps.py
@@ -12,15 +12,17 @@ import astropy.units as u
 import coop_setup_funcs as csf
 import websky as ws
 from astropy.io import fits
+import h5py
 
 # inputs for different options
 # mode   = "peakpatch"
-mode = "redmagic"
+# mode = "redmagic"
 # mode = "maglim"
 #mode = "redmagic_buzz"
-#mode = "maglim_buzz"
+# mode = "maglim_buzz"
+mode = "maglim_cardinal"
 
-split = False
+split = True
 masswgt_odmap = False
 smth_scale    = 0 * u.Mpc
 #45 * u.Mpc
@@ -95,7 +97,19 @@ elif mode == "maglim_buzz":
     dec = np.asarray(dec)
     z   = np.asarray(z)
     w   = 1
-
+    
+elif mode == "maglim_cardinal":
+    print("Maglim Cardinal.")
+    mask_path = "/mnt/raid-cita/mlokken/cardinal/cardinal_maglim_mask.fits"
+    catfile   = "/mnt/raid-cita/mlokken/cardinal/maglim_Cardinal-3_v2.0_Y6a.hdf5"
+    outpath   = "/mnt/raid-cita/mlokken/cardinal/number_density_maps/maglim/"
+    mass_str  = ''
+    
+    with h5py.File(catfile, 'r') as cat:
+        ra, dec, z, w = cat['ra'][:], cat['dec'][:], cat['DNF_ZMEAN'][:], cat['weight'][:]
+        catlen = len(ra)
+    cat.close()
+    
 elif mode == "redmagic_buzz":
     print("RedMaGiC Buzzard.")
     mask_path = "/mnt/raid-cita/mlokken/data/masks/y3_gold_2.2.1_RING_joint_redmagic_v0.5.1_wide_maglim_v2.2_mask_hpx_4096.fits"

general_code/combine_regionfiles.py
@@ -6,8 +6,9 @@ import coop_setup_funcs as csf
 from astropy import units as u
 # mode = 'GRF'                                                                                                                                                                                            
 # mode  = 'Buzzard'
-mode = 'ACTxDES'
+# mode = 'ACTxDES'
 # mode   = 'Websky'                                                                                                                                                                                       
+mode = "Cardinal"
 
 if mode == 'ACTxDES':
     outpath     = "/mnt/scratch-lustre/mlokken/stacking/ACTxDES_paper2/"
@@ -17,6 +18,9 @@ if mode == 'Buzzard':
     orient_mode = "maglim"
 if mode == "Websky":
     outpath     = "/mnt/scratch-lustre/mlokken/pkpatch/number_density_maps/fullsky/galaxies/orient_tests/"
+if mode == 'Cardinal':
+    outpath = "/mnt/scratch-lustre/mlokken/stacking/Cardinal_paper2/"
+    orient_mode = "maglim"
 
 path = os.path.join(outpath, "temp_reg")
 width    = 200
@@ -97,7 +101,7 @@ for nbin in nlow_hi_bins:
     print("dlow, dhi", dlow_abs, dhi_abs)
     for file in os.listdir(path):
         # check if it's a galaxy ndmap stack
-        if (f"{cl_dlow_abs}_{cl_dhi_abs}") in file and (("DES_maglim" in file) or ("Buzzard_maglim" in file)):
+        if (f"{cl_dlow_abs}_{cl_dhi_abs}") in file and (("DES_maglim" in file) or ("Buzzard_maglim" in file) or ("Cardinal_maglim" in file)):
             combining = True
             f = np.load(os.path.join(path,file), allow_pickle=True)
             for key in f.keys():

general_code/coop_post_processing.py
@@ -67,13 +67,15 @@ def get_peakinfo(filename):
     peakfile = fits.open(filename)
     peakinfo = peakfile[0].data
     rot_angle = peakinfo[:,3]
+    peakid    = peakinfo[:,0]
     theta,phi = peakinfo[:,1], peakinfo[:,2]
-    parity   = peakinfo[:,5]
+    parityx   = peakinfo[:,4]
+    parityy   = peakinfo[:,5]
     dec, ra = ef.ThetaPhitoDeclRa(theta,phi)
     # ra = np.asarray(ra)
     # dec = np.asarray(dec)
     peakfile.close()
-    return (rot_angle,ra,dec, parity)
+    return (rot_angle,ra,dec, parityx, parityy, peakid)
     
 def get_vector_components(rot_angle):
     U_arr = np.zeros(len(rot_angle))
@@ -550,6 +552,79 @@ def hankel_multi_same_csize(hankel_list, npks_list, d_low, slice_width, arcmin_p
         c += 1
     return hankel_array / sum(npks_list), rad_in_mpc
 
+def integrate(profile, r, upper_bound, errors=None):
+    reimann_sum = 0
+    error_sum   = 0
+    delta_r = r[1]-r[0]
+    ub_idx = (np.abs(r - upper_bound)).argmin()
+    for i in range(ub_idx+1):    
+        reimann_sum += r[i]*profile[i]*delta_r
+        if errors is not None:
+            error_sum += (delta_r*r[i]*errors[i])**2
+    return(reimann_sum*np.pi, np.sqrt(error_sum)*np.pi)
+
+
+def retrieve_stack_info(path, mapstr, pt_selection_str, cl_dbin, cutmin=20, pct=75, orient_mode='maglim', xyup=True, binsize=7.5, crop_center=2.5, scale=None, make_delta=False, remove_r30=False, nreg=24):
+    if xyup:
+        xyupstr = '_orientXYUP'
+    else:
+        xyupstr = ''
+    
+    cl_dlow, cl_dhi = cl_dbin[0], cl_dbin[1] # extent of the clusters
+    
+    g_dlow   = cl_dlow-50 # extent of the galaxies
+    g_dhi    = cl_dhi+50
+    zlow   = z_at_value(cosmo.comoving_distance, cl_dlow*u.Mpc)
+    zhi    = z_at_value(cosmo.comoving_distance, cl_dhi*u.Mpc)
+    
+    file = path + f"{mapstr}_redmapper_lambdagt{cutmin}_combined_{cl_dlow}_{cl_dhi}Mpc_{pt_selection_str}20pt0{xyupstr}_{pct}pct_{orient_mode}_{g_dlow}_{g_dhi}Mpc_{nreg}reg_m0to5_profiles.pkl"
+    print(f"retrieving data from {file}")
+    stack_info = np.load(file, allow_pickle=True)
+    # rearrange shape of profs to be compatible with Stack_object
+    profs = np.transpose(np.asarray(stack_info['prof']), axes=[2,0,1])
+    if crop_center is not None:
+        rmax = len(profs[0,0,:])
+        r_in_mpc = 40/rmax*np.arange(rmax)
+        idx_crop = np.where(np.abs(r_in_mpc-crop_center) == np.min(np.abs(r_in_mpc-crop_center)))[0][0]
+        # cut off profiles up to 2.5 Mpc
+        profs = profs[:,:,idx_crop:]
+    
+    if scale is not None:
+        profs *= scale
+    stack_obj = Stack_object(img_splits=stack_info['stacks'], profile_splits=profs, Npks_splits=stack_info['npks_list'], rad_in_Mpc=40)
+    if crop_center is not None:
+        # reset r of stack_obj to account for 2.5 Mpc cut
+        stack_obj.r = stack_obj.r[idx_crop:]
+    
+    if make_delta:
+        # find the average matter density over the extent of the redshift range
+        rholist = []
+        # plt.figure(figsize=(10,10))
+        for chi in np.linspace(cl_dlow, cl_dhi,100):
+            chibinsize = (cl_dhi-cl_dlow)/100
+            zmid = z_at_value(cosmo.comoving_distance, (chi+chibinsize)/2*u.Mpc)
+            rho_avg_chi = cosmo.Om(z=zmid)* cosmo.critical_density0.to(u.Msun/u.Mpc**3)
+            # multiply it by the bin size to get the average surface mass density in the bin
+            rho_avg_bin = rho_avg_chi * chibinsize * u.Mpc
+            rholist.append(rho_avg_bin.value)
+        avg_matter = np.average(rholist)
+        print("average rho in this bin", avg_matter)
+        stack_obj.profile_splits[:,:,:] /= avg_matter
+        # convert 2D delta to 3D delta by estimating length of object along line of sight
+        est_los = 10 # Mpc
+        stack_obj.profile_splits[:,:,:] /= est_los
+    if remove_r30:
+        idx_r30 = np.where(np.abs(stack_obj.r-30) == np.min(np.abs(stack_obj.r-30)))[0][0]
+        for r in range(stack_obj.profile_splits.shape[1]):
+            avg_r30 = np.average(stack_obj.profile_splits[0,r,:][idx_r30:])
+            stack_obj.profile_splits[0,r,:] -= avg_r30
+        
+        
+    stack_obj.set_average_profiles()
+    stack_obj.bin_and_get_stats(binsize) #Mpc
+    return stack_obj, (zlow, zhi)
+
+
 class Stack_object:
     # an object to be loaded in from a file of an errors run
     # Not using any Astropy Quantities in this class because they cause bugs
@@ -703,39 +778,3 @@ class Stack_object:
         self.set_covariance_full()
         self.set_covariance_binned()
         
-def retrieve_stack_info(path, mapstr, pt_selection_str, cl_dbin, cutmin=20, pct=75, orient_mode='maglim', xyup=True, binsize=7.5, crop_center=2.5, scale=None, nreg=24):
-    if xyup:
-        xyupstr = '_orientXYUP'
-    else:
-        xyupstr = ''
-    
-    cl_dlow, cl_dhi = cl_dbin[0], cl_dbin[1] # extent of the clusters
-    g_dlow   = cl_dlow-50 # extent of the galaxies
-    g_dhi    = cl_dhi+50
-    zlow   = z_at_value(cosmo.comoving_distance, cl_dlow*u.Mpc)
-    zhi    = z_at_value(cosmo.comoving_distance, cl_dhi*u.Mpc)
-    file = path + f"{mapstr}_redmapper_lambdagt{cutmin}_combined_{cl_dlow}_{cl_dhi}Mpc_{pt_selection_str}20pt0{xyupstr}_{pct}pct_{orient_mode}_{g_dlow}_{g_dhi}Mpc_{nreg}reg_m0to5_profiles.pkl"
-    stack_info = np.load(file, allow_pickle=True)
-    # rearrange shape of profs to be compatible with Stack_object
-    profs = np.transpose(np.asarray(stack_info['prof']), axes=[2,0,1])
-    if crop_center is not None:
-        rmax = len(profs[0,0,:])
-        r_in_mpc = 40/rmax*np.arange(rmax)
-        idx_2p5 = np.where(np.abs(r_in_mpc-2.5) == np.min(np.abs(r_in_mpc-2.5)))[0][0]
-        print("2.5 index", idx_2p5)
-        # cut off profiles up to 2.5 Mpc
-        profs = profs[:,:,idx_2p5:]
-        
-    if scale is not None:
-        profs *= scale
-    stack_obj = Stack_object(img_splits=stack_info['stacks'], profile_splits=profs, Npks_splits=stack_info['npks_list'], rad_in_Mpc=40)
-    idx_r30 = np.where(np.abs(stack_obj.r-30) == np.min(np.abs(stack_obj.r-30)))[0][0]
-    if crop_center is not None:
-        # reset r of stack_obj to account for 2.5 Mpc cut
-        stack_obj.r = stack_obj.r[idx_2p5:]
-
-    for r in range(stack_obj.profile_splits.shape[1]):
-        stack_obj.profile_splits[0,r,:] -= np.average(stack_obj.profile_splits[0,r,:][idx_r30:])
-    stack_obj.set_average_profiles()
-    stack_obj.bin_and_get_stats(binsize) #Mpc
-    return stack_obj, (zlow, zhi)

general_code/coop_setup_funcs.py
@@ -131,7 +131,7 @@ def get_radeczlambda(filepath, min_mass=None, return_mass=False, return_id=False
         ra  = dat['RA']
         dec = dat['DEC']
         z = dat['Z_LAMBDA']
-        if 'buzzard' in filepath:
+        if ('buzzard' in filepath) or ('cardinal' in filepath):
             richness = dat['lambda']
         else:
             richness = dat['lambda_chisq']
@@ -163,7 +163,50 @@ def get_radeczlambda(filepath, min_mass=None, return_mass=False, return_id=False
     if return_id:
         to_return.append(id)
     return to_return
-    
+
+def get_nu(od_filename, ra, dec, mask=None):
+    print("faster nu")
+    import healpy as hp
+    ''' returns the nu values at the positions of the input ra, dec'''
+    od_map = read_amp_map(od_filename)
+    nside  = hp.get_nside(od_map)
+    pixels = hp.ang2pix(nside, ra, dec, lonlat=True)
+    if mask is not None:
+        mean_od = np.sum(od_map*mask)/np.sum(mask)
+        rms = np.sqrt(np.sum((od_map-mean_od)**2*mask)/np.sum(mask))
+    else:
+        mean_od = np.sum(od_map)/od_map.size
+        rms = np.sqrt(np.sum((od_map-mean_od)**2)/od_map.size)
+    print("rms = ", rms)
+    print("mean of overdensity map = ", mean_od)
+    if mask is not None:
+        nu = ((od_map-mean_od)*mask) / rms
+    else:
+        nu = (od_map-mean_od)/rms
+    hp.mollview(nu)
+    return(nu[pixels])
+
+def get_x_e(e_filename, ra, dec, mask=None):
+    import healpy as hp
+    ecc_map0 = hp.read_map(e_filename, field=0)
+    ecc_map1 = hp.read_map(e_filename, field=1)    
+    ecc_map2 = hp.read_map(e_filename, field=2)
+    nside    = hp.get_nside(ecc_map0)
+    pixels   = hp.ang2pix(nside, ra, dec, lonlat=True)
+    e  = np.sqrt((ecc_map1**2 + ecc_map2**2)/(ecc_map0**2))
+    #ecc_map0 = del^2(F)
+    if mask is None:
+        mean_del2 = np.sum(ecc_map0)/ecc_map0.size
+        rms_2 = np.sqrt(np.sum((ecc_map0-mean_del2)**2)/ecc_map0.size)
+        x = ecc_map0 / rms_2
+    else:
+        mean_del2 = np.sum(ecc_map0*mask)/np.sum(mask)
+        rms_2 = np.sqrt(np.sum((ecc_map0-mean_del2)**2*mask)/np.sum(mask))
+        x = ecc_map0*mask / rms_2
+    print("mean of del^2(F) = ", mean_del2)
+    print("rms of del^2(F)  = ", rms_2)
+    print("Map of x = del^2(F)")
+    return e[pixels], x[pixels]
 
 def mass_to_richness(M, z):
     # M (mass) can either be a single value or array                                                                                                
@@ -301,7 +344,17 @@ def radec_to_thetaphi_sliced(ra, dec, z_arr, minz=None, maxz=None, slice_width=N
         print("Theta phi file saved in %s, full info file saved in %s \n"%(saveas_tp, saveas_full))
 '''
 
+def read_amp_map(amp_file):
+    import healpy as hp
+    ''' reads in an amplitude map and returns it '''
+    # if reading with healpy throws an error, rewrite the header to have the correct TTYPE3
+    if fits.open(amp_file)[1].header['TTYPE3'] != 'ID2':    
+        amp_map = fits.open(amp_file)
+        amp_map[1].header['TTYPE3'] = 'ID2'
+        amp_map.writeto(amp_file, overwrite=True)
+        amp_map.close()
 
+    return hp.read_map(amp_file, field=0)
 
 def tophat_beam(scale, lmax=8000):
     import healpy as hp